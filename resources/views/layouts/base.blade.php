4c9db54c6b8235480b467e7e32ff3aca42b21fa4
Fahmi Muhazir <fahmimuhazir12@gmail.com>
2024-04-28 14:15:03 +0700

    update: style references and add Bootstrap assets in base template

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -16,21 +16,22 @@
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
+  <!-- Template Main JS & CSS File -->
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/vendor/bootstrap/css/bootstrap.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/vendor/bootstrap/js/bootstrap.js') }}">
+<script type="text/javascript" src="{{ asset('assets') }}/js/jquery-3.3.1.min.js"></script>
+
   <!-- Vendor CSS Files -->
   <link href="{{ asset('assets/vendor/animate.css/animate.css') }}" rel="stylesheet">
   <link href="{{ asset('assets/vendor/aos/aos.js')}}" rel="stylesheet">
-  <link href="{{ asset('assets/css/bootstrap.css') }}" rel="stylesheet">
   <link href="{{ asset('assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
   <link href="{{ asset('assets/vendor/boxicons/css/boxicons.css') }}" rel="stylesheet">
-  {{-- <link href="{{ asset('assets') }}/vendor/glightbox/css/glightbox.min.css" rel="stylesheet"> --}}
   <link href="{{ asset('assets/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
   <link href="{{ asset('assets/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
 
-  <!-- Template Main CSS File -->
+  <!-- Template Custom CSS File -->
   <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
 
-  <link rel="stylesheet" type="text/css" href="{{ asset('assets/js/bootstrap.js') }}">
-  <script type="text/javascript" src="{{ asset('assets') }}/js/jquery-3.3.1.min.js"></script>
 
 </head>
 
@@ -38,23 +39,23 @@
 
   @include('layouts.header')
 
-  @yield('content')
-  {{-- <div class="col-md-9 text-center mx-auto">
-    @yield('form')
-  </div> --}}
+    <div class="main min-vh-100">
+        @yield('content')
+
+    </div>
 
   @include('layouts.footer')
 
   <!-- Vendor JS Files -->
   <script src="{{ asset('assets/vendor/aos/aos.js') }}"></script>
-  <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+  {{-- <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script> --}}
   {{-- <script src="assets/vendor/glightbox/js/glightbox.min.js"></script> --}}
   <script src="{{ asset('assets/vendor/isotope-layout/isotope.pkgd.js') }}"></script>
   <script src="{{ asset('assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
   <script src="{{ asset('assets/vendor/waypoints/noframework.waypoints.js') }}"></script>
   <script src="{{ asset('assets/vendor/php-email-form/validate.js') }}"></script>
 
-  <!-- Template Main JS File -->
+  <!-- Template Custom JS File -->
   <script src="{{ asset('assets/js/main.js') }}"></script>
 
 </body>
